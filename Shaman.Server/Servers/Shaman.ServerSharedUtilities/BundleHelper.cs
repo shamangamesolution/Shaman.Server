5897f39f42f6d5f2fa29d07f0ad701f193ee554a
Labor Dmitriy <labor.d@azurgames.com>
2019-11-28 17:46:35 +0300

    add zipped-http bundle sourcing

--- Shaman.Server/Servers/Shaman.ServerSharedUtilities/BundleHelper.cs
+++ Shaman.Server/Servers/Shaman.ServerSharedUtilities/BundleHelper.cs
@@ -1,20 +1,52 @@
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 
 namespace Shaman.ServerSharedUtilities
 {
     public class BundleHelper
     {
-        public static T LoadTypeFromBundle<T>(string publishDir)
+        public static T LoadTypeFromBundle<T>(string uri)
+        {
+            return uri.StartsWith("http") ? LoadTypeFromHttpBundle<T>(uri) : LoadTypeFromLocalBundle<T>(uri);
+        }
+
+        private static T LoadTypeFromHttpBundle<T>(string url)
+        {
+            var uri = new Uri(url);
+
+            var bundlesFolder = Path.Combine(Path.GetTempPath(), "shaman.bundles");
+            var bundleDest = Path.Combine(bundlesFolder, uri.Segments.Last());
+
+            if (!Directory.Exists(bundlesFolder))
+                Directory.CreateDirectory(bundlesFolder);
+            
+            var newBundleFolder = Path.Combine(bundlesFolder,Path.GetFileNameWithoutExtension(bundleDest));
+            if (!Directory.Exists(newBundleFolder))
+            {
+                Console.Out.WriteLine($"Downloading bundle from {uri}");
+                using (var wc = new WebClient())
+                    wc.DownloadFile(uri, bundleDest);
+                ZipFile.ExtractToDirectory(bundleDest, newBundleFolder);
+                File.Delete(bundleDest);
+            }
+
+            Console.Out.WriteLine($"Using bundle from {newBundleFolder}");
+            
+            return (T) Activator.CreateInstance(LoadAndGet<T>(newBundleFolder));
+        }
+
+        private static T LoadTypeFromLocalBundle<T>(string publishDir)
         {
             return (T) Activator.CreateInstance(LoadAndGet<T>(publishDir));
         }
 
         private static Type LoadAndGet<T>(string publishDir)
         {
-            var files = Directory.GetFiles(publishDir).Where(f=>f.EndsWith(".dll"));
+            var files = Directory.GetFiles(publishDir).Where(f => f.EndsWith(".dll"));
             Type targetType = null;
             foreach (var s in files)
             {
@@ -24,9 +56,10 @@ namespace Shaman.ServerSharedUtilities
                     var assembly = Assembly.LoadFrom(s);
                     if (targetType == null)
                     {
-                        targetType = assembly.GetTypes().SingleOrDefault(t => t.GetInterfaces().Any(obj => obj == typeof(T)));    
+                        targetType = assembly.GetTypes()
+                            .SingleOrDefault(t => t.GetInterfaces().Any(obj => obj == typeof(T)));
                     }
-                    
+
                     Console.Out.WriteLine("Assembly = {0}", assembly.FullName);
                 }
                 catch (FileLoadException e)
@@ -42,7 +75,7 @@ namespace Shaman.ServerSharedUtilities
                     throw;
                 }
             }
-            
+
             if (targetType == null)
             {
                 throw new BundleLoadException(
