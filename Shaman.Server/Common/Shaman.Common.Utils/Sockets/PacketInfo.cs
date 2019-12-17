e4ec4f389c798b1790deb6a333507152ac56be70
Labor Dmitriy <labor.d@azurgames.com>
2019-12-17 17:23:13 +0300

    extend packet sender with offset&length

--- Shaman.Server/Common/Shaman.Common.Utils/Sockets/PacketInfo.cs
+++ Shaman.Server/Common/Shaman.Common.Utils/Sockets/PacketInfo.cs
@@ -45,7 +45,11 @@ namespace Shaman.Common.Utils.Sockets
         public int Offset{get;}
         public int Length{get; private set; }
 
-        public PacketInfo(byte[] data, bool isReliable, bool isOrdered, int maxPacketSize)
+        public PacketInfo(byte[] data, bool isReliable, bool isOrdered, int maxPacketSize) : this(data, 0, data.Length,
+            isReliable, isOrdered, maxPacketSize)
+        {
+        }
+        public PacketInfo(byte[] data,int offset, int length, bool isReliable, bool isOrdered, int maxPacketSize)
         {
             Buffer = ArrayPool<byte>.Shared.Rent(Math.Max(maxPacketSize, data.Length + 3));
             Length = 1 /* packet number byte */;
@@ -54,26 +58,29 @@ namespace Shaman.Common.Utils.Sockets
             Buffer[0] = 1;
             IsReliable = isReliable;
             IsOrdered = isOrdered;
-            AddData(data);
+            AddData(data, offset, length);
         }
 
         public void Append(byte[] serializedMessage)
         {
             Buffer[0]++;
-            AddData(serializedMessage);
+            AddData(serializedMessage, 0, serializedMessage.Length);
+        }
+        public void Append(byte[] serializedMessage, int offset, int length)
+        {
+            Buffer[0]++;
+            AddData(serializedMessage, offset, length);
         }
 
-        private void AddData(byte[] serializedMessage)
+        private void AddData(byte[] serializedMessage, int offset, int length)
         {
-            var serializedLength = (ushort) serializedMessage.Length;
+            Buffer[Length] = (byte) length;
+            Buffer[Length + 1] = (byte) (length >> 8);
 
-            Buffer[Length] = (byte) serializedLength;
-            Buffer[Length + 1] = (byte) (serializedLength >> 8);
-
-            System.Buffer.BlockCopy(serializedMessage, 0, Buffer, Length + sizeof(ushort),
-                serializedLength);
+            System.Buffer.BlockCopy(serializedMessage, offset, Buffer, Length + sizeof(ushort),
+                length);
                 
-            Length += sizeof(ushort) + serializedLength;
+            Length += sizeof(ushort) + length;
         }
 
         public void Dispose()
