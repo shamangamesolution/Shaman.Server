109a17805801c9a1fdc2e56b4ed6c9a4f0cd777b
Max Tiganov <mtiganov@gmail.com>
2019-12-18 12:11:40 +0300

    urlhelper fix

--- Shaman.Server/Common/Shaman.Common.Utils/Helpers/UrlHelper.cs
+++ Shaman.Server/Common/Shaman.Common.Utils/Helpers/UrlHelper.cs
@@ -4,7 +4,7 @@ namespace Shaman.Common.Utils.Helpers
     {
         public static string GetUrl(int httpPort, int httpsPort, string address)
         {
-            var protocol = (httpPort > 0) ? "https" : "http";
+            var protocol = (httpsPort > 0) ? "https" : "http";
             var port = (httpsPort > 0) ? httpsPort : httpPort;
             
             return $"{protocol}://{address}:{port}";
