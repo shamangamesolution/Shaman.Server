daa389e114f90cbe79c524d6c05af99925290f43
Max Tiganov <mtiganov@gmail.com>
2020-06-17 18:46:13 +0300

    +mongo driver wrapper for EntityBase

---
+++ Shaman.Server/Database/Shaman.DAL.MongoDb/IMongoDbMapper.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson.Serialization;
+using Shaman.Common.Utils.Messages;
+
+namespace Shaman.DAL.MongoDb
+{
+    public interface IMongoDbMapper
+    {
+        string GetCollectionName();
+    }
+
+    public class DefaultMapper<T> : IMongoDbMapper where T:EntityBase    
+    {
+        public DefaultMapper()
+        {
+            // BsonClassMap.RegisterClassMap<T>(map =>
+            // {
+            //     map.AutoMap();
+            //     map.SetIgnoreExtraElements(true);
+            //     map.MapIdMember(x => x.Id);
+            // });
+        }
+        
+        public string GetCollectionName()
+        {
+            return typeof(T).Name;
+        }
+    }
+}
