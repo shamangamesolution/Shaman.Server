b1f99c0db49192fec99ef304467a43746f22e1b5
Labor Dmitriy <labordv@gmail.com>
2020-07-22 20:17:59 +0300

    test stuff

--- Shaman.Server/Tests/Shaman.Tests/Helpers/PackageHelper.cs
+++ Shaman.Server/Tests/Shaman.Tests/Helpers/PackageHelper.cs
@@ -1,3 +1,5 @@
+using Shaman.Common.Contract;
+using Shaman.Common.Utils.Logging;
 using Shaman.Common.Utils.Messages;
 using Shaman.Common.Utils.Serialization;
 using Shaman.Common.Utils.Sockets;
@@ -6,13 +8,19 @@ namespace Shaman.Tests.Helpers
 {
     public static class PackageHelper
     {
+        private static readonly ConsoleLogger ConsoleLogger = new ConsoleLogger();
+
         public static DataPacket GetPacketInfo(MessageBase message)
         {
             var serializerFactory = new BinarySerializer();
             var initMsgArray = serializerFactory.Serialize(message); //message.Serialize(_serializerFactory);
-            var packetInfo = new PacketInfo(initMsgArray, false, false, 300);
 
-            return new DataPacket(packetInfo.Buffer, 0, packetInfo.Length, message.IsReliable);
+
+            var packetInfo = new PacketInfo(new DeliveryOptions(false, false), 300, ConsoleLogger,
+                new Payload(initMsgArray));
+
+            return new DataPacket(packetInfo.Buffer, 0, packetInfo.Length,
+                new DeliveryOptions(message.IsReliable, message.IsOrdered));
         }
     }
 }
