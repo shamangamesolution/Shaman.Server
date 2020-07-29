a7143c75501945151e7b47f6b0998f6e3cb17f07
Labor Dmitriy <labordv@gmail.com>
2020-07-29 17:50:07 +0300

    extract sender logic a bit (prepare for reusing in bundle)

--- Shaman.Server/Common/Shaman.Common.Utils/Senders/ShamanSender.cs
+++ Shaman.Server/Common/Shaman.Common.Utils/Senders/ShamanSender.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Shaman.Common.Contract;
+using Shaman.Common.Contract.Logging;
 using Shaman.Common.Utils.Logging;
-using Shaman.Common.Utils.Messages;
 using Shaman.Common.Utils.Peers;
 using Shaman.Common.Utils.Serialization;
-using Shaman.Common.Utils.Serialization.Pooling;
+using Shaman.Serialization;
+using Shaman.Serialization.Messages;
 
 namespace Shaman.Common.Utils.Senders
 {
@@ -17,85 +18,25 @@ namespace Shaman.Common.Utils.Senders
         void CleanupPeerData(IPeerSender peer);
     }
 
-    public class ShamanSender : IShamanSender
+    public class ShamanSender : ShamanSenderBase<IPeerSender>, IShamanSender
     {
-        private readonly ISerializer _serializer;
         private readonly IPacketSender _packetSender;
-        private readonly IShamanLogger _logger;
-        
-        // todo realy need base packet size?
-        private readonly IPacketSenderConfig _config;
         
         private static readonly ConcurrentDictionary<Type, int> BufferStatistics = new ConcurrentDictionary<Type, int>();
 
         public ShamanSender(ISerializer serializer, IPacketSender packetSender, IShamanLogger logger,
-            IPacketSenderConfig config)
+            IPacketSenderConfig config): base(serializer, logger, config.GetBasePacketBufferSize())
         {
-            _serializer = serializer;
             _packetSender = packetSender;
-            _logger = logger;
-            _config = config;
         }
-
-        public int Send(ISerializable message, DeliveryOptions deliveryOptions, IPeerSender peer)
+        protected override void Send(DeliveryOptions deliveryOptions, IPeerSender peer, Payload payload)
         {
-            using (var memoryStream = new PooledMemoryStream(GetBufferSize(message.GetType()), _logger))
-            {
-                _serializer.Serialize(message, memoryStream);
-                var length = (int) memoryStream.Length;
-                _packetSender.AddPacket(peer, deliveryOptions, new Payload(memoryStream.GetBuffer(), 0, length));
-                UpdateBufferSizeStatistics(message.GetType(), length);
-                return length;
-            }
-        }
-
-        public int Send(ISerializable message, DeliveryOptions deliveryOptions, IEnumerable<IPeerSender> peers)
-        {
-            using (var memoryStream = new PooledMemoryStream(_config.GetBasePacketBufferSize(), _logger))
-            {
-                _serializer.Serialize(message, memoryStream);
-                var length = (int) memoryStream.Length;
-                var buffer = memoryStream.GetBuffer();
-                foreach (var peer in peers)
-                {
-                    _packetSender.AddPacket(peer, deliveryOptions, new Payload(buffer, 0, length));
-                }
-                
-                UpdateBufferSizeStatistics(message.GetType(), length);
-                return length;
-            }
-        }
-
-        private void UpdateBufferSizeStatistics(Type dtoType, int actualSize)
-        {
-            var targetValue = (int) (actualSize * 1.5 / 16 + 1) * 16;// pad to 16
-            
-            if (BufferStatistics.TryGetValue(dtoType, out var statisticsValue))
-            {
-                if (statisticsValue < targetValue)
-                {
-                    BufferStatistics.TryUpdate(dtoType, targetValue, statisticsValue);
-                }
-                
-            }
-            else
-            {
-                BufferStatistics.TryAdd(dtoType, targetValue);
-            }
+            _packetSender.AddPacket(peer, deliveryOptions, payload);
         }
 
         public void CleanupPeerData(IPeerSender peer)
         {
             _packetSender.CleanupPeerData(peer);
         }
-
-        private int GetBufferSize(Type dtoType)
-        {
-            if (BufferStatistics.TryGetValue(dtoType, out var size))
-                return size;
-            return _config.GetBasePacketBufferSize();
-        }
-
-        
     }
 }
