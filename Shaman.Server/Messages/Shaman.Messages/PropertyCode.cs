f3dfb3483f4ca522b5e367833e1236c92a8053c3
Labor Dmitriy <labordv@gmail.com>
2020-08-11 01:40:04 +0300

    remove redundands

--- Shaman.Server/Messages/Shaman.Messages/PropertyCode.cs
+++ Shaman.Server/Messages/Shaman.Messages/PropertyCode.cs
@@ -3,14 +3,6 @@ namespace Shaman.Messages
     public struct PropertyCode
     {
 
-        public struct PlayerProperties
-        {
-            public const byte League = 2;
-            public const byte PlayerPower = 3;
-            public const byte Level = 11;
-            public const byte BackendId = 16;
-            public const byte GameMode = 20;
-        }
 
         public struct RoomProperties
         {
