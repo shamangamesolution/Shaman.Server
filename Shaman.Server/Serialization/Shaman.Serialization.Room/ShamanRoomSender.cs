aefe88930206fd4e7756c35c862c99d6339ef713
Labor Dmitriy <labordv@gmail.com>
2020-08-11 02:08:12 +0300

    complete room sender

--- Shaman.Server/Serialization/Shaman.Serialization.Room/ShamanRoomSender.cs
+++ Shaman.Server/Serialization/Shaman.Serialization.Room/ShamanRoomSender.cs
@@ -18,7 +18,7 @@ namespace Shaman.Serialization.Room
             _shamanStreamPool = new ShamanStreamPool(64);
         }
 
-        protected int Send(ISerializable message, DeliveryOptions deliveryOptions, Guid peer)
+        public int Send(ISerializable message, DeliveryOptions deliveryOptions, Guid peer)
         {
             var stream = _shamanStreamPool.Rent(message.GetType());
             try
@@ -33,13 +33,27 @@ namespace Shaman.Serialization.Room
             }
         }
 
-        protected int SendToAll(ISerializable message, DeliveryOptions deliveryOptions)
+        public int SendToAll(ISerializable message, DeliveryOptions deliveryOptions)
         {
             var stream = _shamanStreamPool.Rent(message.GetType());
             try
             {
                 _serializer.Serialize(message, stream);
                 _roomSender.SendToAll(new Payload(stream.GetBuffer()), deliveryOptions);
+                return (int) stream.Length;
+            }
+            finally
+            {
+                _shamanStreamPool.Return(stream, message.GetType());
+            }
+        }
+        public int SendToAll(ISerializable message, DeliveryOptions deliveryOptions, Guid exception)
+        {
+            var stream = _shamanStreamPool.Rent(message.GetType());
+            try
+            {
+                _serializer.Serialize(message, stream);
+                _roomSender.SendToAll(new Payload(stream.GetBuffer()), deliveryOptions, exception);
                 return (int) stream.Length;
             }
             finally
