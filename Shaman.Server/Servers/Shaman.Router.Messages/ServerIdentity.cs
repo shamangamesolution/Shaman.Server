358aafeaa5c7970cb20d475433b71dce02daa9fc
Labor Dmitriy <labordv@gmail.com>
2020-08-15 20:05:05 +0300

    cleanup serverIdentity

--- Shaman.Server/Servers/Shaman.Router.Messages/ServerIdentity.cs
+++ Shaman.Server/Servers/Shaman.Router.Messages/ServerIdentity.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Shaman.Serialization;
-using Shaman.Serialization.Messages;
 
 namespace Shaman.Router.Messages
 {
-    public class ServerIdentity : EntityBase, IEquatable<ServerIdentity>
+    public class ServerIdentity : ISerializable
     {
         public string Address { get; set; }
         public List<ushort> Ports { get; set; }
-        private Guid _id { get; set; }
         public string PortsString { get; set; }
         public ServerRole ServerRole { get; set; }
 
@@ -50,7 +48,6 @@ namespace Shaman.Router.Messages
             Ports = ports;
             ServerRole = serverRole;
             PortsString = GetAsPortString(Ports);
-            _id = Guid.NewGuid();
         }
         
         public ServerIdentity(string address, string ports, ServerRole serverRole)
@@ -59,7 +56,6 @@ namespace Shaman.Router.Messages
             PortsString = ports;
             ServerRole = serverRole;
             Ports = GetAsUshortList(PortsString).ToList();
-            _id = Guid.NewGuid();
         }
 
         public ServerIdentity()
@@ -78,48 +74,6 @@ namespace Shaman.Router.Messages
             return str;
         }
 
-        protected override void SerializeBody(ITypeWriter typeWriter)
-        {
-            typeWriter.Write((byte)this.ServerRole);
-            typeWriter.Write(this.Address);
-            typeWriter.Write(Ports.Count);
-            foreach(var port in Ports)
-                typeWriter.Write(port);
-            typeWriter.Write(_id);
-            typeWriter.Write(this.PortsString);
-        }
-
-        protected override void DeserializeBody(ITypeReader typeReader)
-        {
-            this.ServerRole = (ServerRole) typeReader.ReadByte();
-            this.Address = typeReader.ReadString();
-            var count = typeReader.ReadInt();
-            this.Ports = new List<ushort>();
-            for (var i = 0; i < count; i++)
-                this.Ports.Add(typeReader.ReadUShort());
-            _id = typeReader.ReadGuid();
-            PortsString = typeReader.ReadString();
-        }
-        
-        public class Equali2tyComparer : IEqualityComparer<ServerIdentity> {
-
-            public bool Equals(ServerIdentity x, ServerIdentity y)
-            {
-                if (x == null && y == null)
-                    return true;
-                if (x == null || y == null)
-                    return false;
-
-                return x._id == y._id;
-            }
-
-            public int GetHashCode(ServerIdentity x)
-            {
-                return x._id.GetHashCode();
-            }
-
-        }
-
         public bool Equals(ServerIdentity other)
         {
             if (ReferenceEquals(null, other)) return false;
@@ -129,25 +83,25 @@ namespace Shaman.Router.Messages
                    string.Equals(PortsString, other.PortsString) && ServerRole == other.ServerRole;
         }
 
-        public override bool Equals(object obj)
+        public void Serialize(ITypeWriter typeWriter)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((ServerIdentity) obj);
+            typeWriter.Write((byte)this.ServerRole);
+            typeWriter.Write(this.Address);
+            typeWriter.Write(Ports.Count);
+            foreach(var port in Ports)
+                typeWriter.Write(port);
+            typeWriter.Write(this.PortsString);
         }
 
-        public override int GetHashCode()
+        public void Deserialize(ITypeReader typeReader)
         {
-            unchecked
-            {
-                var hashCode = (Address != null ? Address.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (Ports != null ? Ports.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ _id.GetHashCode();
-                hashCode = (hashCode * 397) ^ (PortsString != null ? PortsString.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (int) ServerRole;
-                return hashCode;
-            }
+            this.ServerRole = (ServerRole) typeReader.ReadByte();
+            this.Address = typeReader.ReadString();
+            var count = typeReader.ReadInt();
+            this.Ports = new List<ushort>();
+            for (var i = 0; i < count; i++)
+                this.Ports.Add(typeReader.ReadUShort());
+            PortsString = typeReader.ReadString();
         }
     }
 }
