a406b4db4b2c0cc04ba3b3d8a29f6fb49167d07f
Labor Dmitriy <labordv@gmail.com>
2020-08-16 13:22:56 +0300

    a bit cleanup

--- Shaman.Server/Contracts/Shaman.Contract.Common/Logging/ILogger.cs
+++ Shaman.Server/Contracts/Shaman.Contract.Common/Logging/ILogger.cs
@@ -9,22 +9,18 @@ namespace Shaman.Contract.Common.Logging
         Info,
         Debug
     }
-    public enum SourceType
-    {
-        Router,
-        BackEnd,
-        GameServer,
-        MatchMaker
-    }
     public interface IShamanLogger
     {
         void Error(string message);
         void Error(Exception ex);
         void LogInfo(string message);
         void LogDebug(string message);
+        [Obsolete("Use one-argument overload")]
         void LogInfo(string sourceName, string action, string message);
+        [Obsolete("Use one-argument overload")]
         void LogWarning(string sourceName, string action, string message);        
         void LogWarning(string message);        
+        [Obsolete("Use one-argument overload")]
         void Error(string sourceName, string action, string message);
     }
 }
