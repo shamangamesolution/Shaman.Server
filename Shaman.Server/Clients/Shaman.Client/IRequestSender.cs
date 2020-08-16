ca0bb758d34f59809f0e8fe4408782e4decdc1bd
Labor Dmitriy <labordv@gmail.com>
2020-08-16 20:16:59 +0300

    restore test bundle stuff

--- Shaman.Server/Clients/Shaman.Client/IRequestSender.cs
+++ Shaman.Server/Clients/Shaman.Client/IRequestSender.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Shaman.Serialization.Messages.Http;
 
-namespace Shaman.Common.Http
+namespace Shaman.Client
 {
     public interface IRequestSender
     {
