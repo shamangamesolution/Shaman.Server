bb580fba46aefd56d63d3792a3ab5e292448ea75
Max Tiganov <mtiganov@gmail.com>
2020-09-21 11:48:20 +0300

    moved some Mongo code related to EntityBase to this repo from Shaman.DAL

---
+++ Shaman.Server/Database/Shaman.DAL.MongoDb.Tools.Tests/TestEntity.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Shaman.Serialization;
+using Shaman.Serialization.Messages;
+
+namespace Shaman.DAL.MongoDb.Tools.Tests
+{
+    public class TestChildEntity : EntityBase
+    {
+        public TestChildEntity(int id, bool boolField, float floatField)
+        {
+            Id = id;
+            BoolField = boolField;
+            FloatField = floatField;
+        }
+
+        public bool BoolField { get; set; }
+        public float FloatField { get; set; }
+
+        protected override void SerializeBody(Serialization.ITypeWriter typeWriter)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DeserializeBody(Serialization.ITypeReader typeReader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+    public class TestEntity : EntityBase
+    {
+        public string StringId { get; set; }
+        public TestEntity(int id, int intField, string stringField)
+        {
+            Id = id;
+            IntField = intField;
+            StringField = stringField;
+            ChildList = new List<TestChildEntity>();
+            ChildDictionary = new EntityDictionary<TestChildEntity>();
+        }
+
+        public int IntField { get; set; }
+        public string StringField { get; set; }
+        
+        public List<TestChildEntity> ChildList { get; set; }
+        public EntityDictionary<TestChildEntity> ChildDictionary { get; set; }
+
+        protected override void SerializeBody(ITypeWriter typeWriter)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DeserializeBody(ITypeReader typeReader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
