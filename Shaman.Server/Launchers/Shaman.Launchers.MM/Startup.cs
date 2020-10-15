e5e7e79a4b91b6d1763af0cf5de90f275df0862f
Labor Dmitriy <labordv@gmail.com>
2020-10-15 12:29:11 +0300

    fix accidentaly change

--- Shaman.Server/Launchers/Shaman.Launchers.MM/Startup.cs
+++ Shaman.Server/Launchers/Shaman.Launchers.MM/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -43,7 +44,9 @@ namespace Shaman.Launchers.MM
             services.AddSingleton<IRoomApiProvider, DefaultRoomApiProvider>();
             //used for configuration of bundle related services
             services.AddSingleton<IDefaultBundleInfoConfig, DefaultBundleInfoConfig>(c =>
-                new DefaultBundleInfoConfig(Configuration["LauncherSettings:BundleUri"], Configuration["CommonSettings:ServerRole"]));
+                new DefaultBundleInfoConfig(Configuration["LauncherSettings:BundleUri"],
+                    Convert.ToBoolean(Configuration["LauncherSettings:OverwriteDownloadedBundle"]),  Configuration[
+                "CommonSettings:ServerRole"]));
             //gets information about bundle - its location
             services.AddSingleton<IBundleInfoProvider, DefaultBundleInfoProvider>();
             //load bundle based on info from IBundleInfoProvider
