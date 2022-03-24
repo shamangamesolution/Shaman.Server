18e6e0099641a87dc39d425af84adf5868f4f885
Labor Dmitriy <labordv@gmail.com>
2022-03-24 13:14:18 +0200

    sql template

---
+++ Shaman.Server/Servers/Shaman.Router/router.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `bundles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `server_id` int(11) NOT NULL,
+  `uri` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `servers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(256) NOT NULL DEFAULT '',
+  `ports` varchar(64) NOT NULL DEFAULT '',
+  `http_port` smallint(6) NOT NULL DEFAULT '0',
+  `https_port` smallint(6) NOT NULL DEFAULT '0',
+  `server_role` tinyint(4) NOT NULL,
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `region` varchar(45) NOT NULL DEFAULT '',
+  `client_version` varchar(45) NOT NULL DEFAULT '',
+  `actualized_on` datetime DEFAULT NULL,
+  `peers_count` int(11) NOT NULL DEFAULT '0',
+  `is_approved` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
