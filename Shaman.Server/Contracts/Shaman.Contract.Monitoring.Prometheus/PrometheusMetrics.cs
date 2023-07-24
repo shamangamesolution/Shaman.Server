792acbdc0f6341d61cfe0dcc9f358d2b0e1853d9
Dmitriy Labor <ldv@bastard.games>
2023-07-24 17:45:28 +0300

    actualize prometheus metrics

--- Shaman.Server/Contracts/Shaman.Contract.Monitoring.Prometheus/PrometheusMetrics.cs
+++ Shaman.Server/Contracts/Shaman.Contract.Monitoring.Prometheus/PrometheusMetrics.cs
@@ -9,30 +9,42 @@ public class PrometheusMetrics : IGameMetrics
     private static readonly Gauge Rooms = Metrics.CreateGauge("rooms", "Rooms");
 
     private static readonly Histogram MaxSendQueueSize =
-        Metrics.CreateHistogram("max_send_queue_size", "Max send queue size");
+        Metrics.CreateHistogram("max_send_queue_size", "Max send queue size", new HistogramConfiguration
+        {
+            Buckets = new double[] {0, 1, 2, 3, 5, 7, 10, 13, 20, 30, 50}
+        });
 
     private static readonly Histogram AvgSendQueueSize =
-        Metrics.CreateHistogram("average_send_queue_size", "Average send queue size");
+        Metrics.CreateHistogram("average_send_queue_size", "Average send queue size", new HistogramConfiguration
+        {
+            Buckets = new double[] {0, 1, 2, 3, 5, 7, 10, 13, 20, 30, 50}
+        });
 
-    private static readonly Histogram RoomLiveTime = Metrics.CreateHistogram("room_live_time", "Room live time");
+    private static readonly Histogram RoomLiveTime = Metrics.CreateHistogram("room_live_time", "Room live time",
+        new HistogramConfiguration
+        {
+            Buckets = new double[] {1, 2, 5, 10, 20, 50, 100, 200, 400, 600, 800, 1000, 1500, 2000, 3000}
+        });
 
-    private static readonly Histogram RoomTrafficSent =
-        Metrics.CreateHistogram("room_traffic_sent", "Room traffic sent");
+    private static readonly Counter RoomTrafficSent = Metrics.CreateCounter("room_traffic_sent", "Room traffic sent");
 
-    private static readonly Histogram RoomTrafficReceived =
-        Metrics.CreateHistogram("room_traffic_received", "Room traffic received");
+    private static readonly Counter RoomTrafficReceived =
+        Metrics.CreateCounter("room_traffic_received", "Room traffic received");
 
-    private static readonly Histogram RoomTotalMessagesReceived =
-        Metrics.CreateHistogram("room_messages_received", "Room messages received");
+    private static readonly Counter RoomTotalMessagesReceived =
+        Metrics.CreateCounter("room_messages_received", "Room messages received");
 
-    private static readonly Histogram RoomTotalMessagesSent =
-        Metrics.CreateHistogram("room_messages_sent", "Room messages sent");
+    private static readonly Counter RoomTotalMessagesSent =
+        Metrics.CreateCounter("room_messages_sent", "Room messages sent");
 
     private static readonly Histogram MaxSendTickDuration =
-        Metrics.CreateHistogram("max_send_tick_duration", "Max send tick duration");
+        Metrics.CreateHistogram("max_send_tick_duration", "Max send tick duration", new HistogramConfiguration
+        {
+            Buckets = new double[] {0, 1, 3, 7, 11, 15, 20, 30, 50, 70, 100, 300, 1000}
+        });
 
-    private static readonly Histogram PacketSenderPeers =
-        Metrics.CreateHistogram("packet_sender_peers", "Packet sender peers");
+    private static readonly Gauge PacketSenderPeers =
+        Metrics.CreateGauge("packet_sender_peers", "Packet sender peers");
 
     private IMetricServer _metricServer;
 
@@ -78,32 +90,32 @@ public class PrometheusMetrics : IGameMetrics
 
     public void TrackRoomTotalTrafficSent(int bytes)
     {
-        RoomTrafficSent.Observe(bytes);
+        RoomTrafficSent.Inc(bytes);
     }
 
     public void TrackRoomTotalTrafficReceived(int bytes)
     {
-        RoomTrafficReceived.Observe(bytes);
+        RoomTrafficReceived.Inc(bytes);
     }
 
     public void TrackRoomTotalMessagesSent(int count)
     {
-        RoomTotalMessagesSent.Observe(count);
+        RoomTotalMessagesSent.Inc(count);
     }
 
     public void TrackRoomTotalMessagesReceived(int count)
     {
-        RoomTotalMessagesReceived.Observe(count);
+        RoomTotalMessagesReceived.Inc(count);
     }
 
     public void TrackRoomTotalMessagesSent(int count, string messageName)
     {
-        RoomTotalMessagesSent.WithLabels(messageName).Observe(count);
+        RoomTotalMessagesSent.WithLabels(messageName).Inc(count);
     }
 
     public void TrackRoomTotalMessagesReceived(int count, string messageName)
     {
-        RoomTotalMessagesReceived.WithLabels(messageName).Observe(count);
+        RoomTotalMessagesReceived.WithLabels(messageName).Inc(count);
     }
 
     public void TrackSendTickDuration(int maxDurationForSec, string listenerTag)
@@ -113,6 +125,6 @@ public class PrometheusMetrics : IGameMetrics
 
     public void TrackSendersCount(string source, int count)
     {
-        PacketSenderPeers.Observe(count);
+        PacketSenderPeers.Inc(count);
     }
 }
