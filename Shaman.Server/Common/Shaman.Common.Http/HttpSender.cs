1ebb5a5f782620c88e6117246a72db20247a52dc
Dmitriy Labor <ldv@bastard.games>
2023-10-12 20:46:10 +0300

    reuse tcp connection

--- Shaman.Server/Common/Shaman.Common.Http/HttpSender.cs
+++ Shaman.Server/Common/Shaman.Common.Http/HttpSender.cs
@@ -10,23 +10,26 @@ using Shaman.Serialization.Messages.Http;
 
 namespace Shaman.Common.Http
 {
-    public class HttpSender: IRequestSender
+    public class HttpSender : IRequestSender
     {
         private readonly IShamanLogger _logger;
         private readonly ISerializer _serializer;
+        private readonly HttpClient _client;
 
         static HttpSender()
         {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11;
         }
-        
+
         public HttpSender(IShamanLogger logger, ISerializer serializer)
         {
             _logger = logger;
             _serializer = serializer;
+            _client = HttpClientFactory.Create();
+            _client.Timeout = new TimeSpan(0, 0, 15);
         }
-        
+
         public async Task<T> SendRequest<T>(string serviceUrl, HttpRequestBase request)
             where T : HttpResponseBase, new()
         {
@@ -37,39 +40,33 @@ namespace Shaman.Common.Http
             try
             {
                 if (string.IsNullOrWhiteSpace(serviceUrl))
-                    throw new Exception($"Base Url address is empty for {request.GetType()}");                   
-                    
+                    throw new Exception($"Base Url address is empty for {request.GetType()}");
+
                 if (string.IsNullOrWhiteSpace(request.EndPoint))
                     throw new Exception($"Request endpoint Url address is empty for {request.GetType()}");
-                
-                using (var client = HttpClientFactory.Create())
+
+                var byteContent = new ByteArrayContent(_serializer.Serialize(request));
+                using (var message = await _client.PostAsync(requestUri, byteContent))
                 {
-                    client.Timeout = new TimeSpan(0, 0, 15);
-                    
-                    var byteContent = new ByteArrayContent(_serializer.Serialize(request));
-
-                    using (var message = await client.PostAsync(requestUri, byteContent))
+                    if (!message.IsSuccessStatusCode)
                     {
-                        if (!message.IsSuccessStatusCode)
-                        {
-                            _logger.Error($"SendRequest {request.GetType()} to {requestUri} error ({stopwatch.ElapsedMilliseconds}ms): {message.StatusCode}");                            
+                        _logger.Error($"SendRequest {request.GetType()} to {requestUri} error ({stopwatch.ElapsedMilliseconds}ms): {message.StatusCode}");
 
-                            responseObject.ResultCode = ResultCode.SendRequestError;
-                        }
-                        else
-                        {
-                            var contentStream = await message.Content.ReadAsStreamAsync();
+                        responseObject.ResultCode = ResultCode.SendRequestError;
+                    }
+                    else
+                    {
+                        using(var contentStream = await message.Content.ReadAsStreamAsync())
                             responseObject = _serializer.DeserializeAs<T>(contentStream);
-                        }
                     }
                 }
             }
             catch (Exception e)
             {
                 _logger.Error($"SendRequest {request.GetType()}  to {requestUri} error ({stopwatch.ElapsedMilliseconds}ms): {e}");
-                responseObject.ResultCode = ResultCode.SendRequestError;                
+                responseObject.ResultCode = ResultCode.SendRequestError;
             }
-            
+
             return responseObject;
         }
 
@@ -86,6 +83,5 @@ namespace Shaman.Common.Http
                 throw;
             }
         }
-
     }
 }
