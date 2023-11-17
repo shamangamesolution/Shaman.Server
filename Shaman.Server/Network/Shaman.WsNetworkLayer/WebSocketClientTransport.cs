9e07ca5693fd06d55ca8c503a119f4200fa268ab
Dmitriy Labor <ldv@bastard.games>
2023-11-17 17:21:23 +0200

    to block scoped ns

--- Shaman.Server/Network/Shaman.WsNetworkLayer/WebSocketClientTransport.cs
+++ Shaman.Server/Network/Shaman.WsNetworkLayer/WebSocketClientTransport.cs
@@ -6,224 +6,225 @@ using Shaman.Common.Udp.Sockets;
 using Shaman.Contract.Common;
 using Shaman.Contract.Common.Logging;
 
-namespace Bro.WsShamanNetwork;
-
-public class WebSocketClientTransport : ITransportLayer
+namespace Bro.WsShamanNetwork
 {
-    private readonly ITaskScheduler _taskScheduler;
-    private readonly IShamanLogger _logger;
-    private readonly ClientWebSocket _clientWebSocket;
-    private static readonly byte[] PingPongLetter = Encoding.UTF8.GetBytes("p");
-
-    private int _rtt = 0;
-    private bool _waitForPong = false;
-    private DateTime _pingSent = DateTime.UtcNow;
-    private readonly TimeSpan KeepAliveInterval;
-    private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-
-    public WebSocketClientTransport(ITaskScheduler taskScheduler, IShamanLogger logger, TimeSpan keepAliveInterval)
+    public class WebSocketClientTransport : ITransportLayer
     {
-        _taskScheduler = taskScheduler;
-        _logger = logger;
-        _clientWebSocket = new ClientWebSocket();
-        KeepAliveInterval = keepAliveInterval;
-    }
+        private readonly ITaskScheduler _taskScheduler;
+        private readonly IShamanLogger _logger;
+        private readonly ClientWebSocket _clientWebSocket;
+        private static readonly byte[] PingPongLetter = Encoding.UTF8.GetBytes("p");
 
-    #region Client-side
+        private int _rtt = 0;
+        private bool _waitForPong = false;
+        private DateTime _pingSent = DateTime.UtcNow;
+        private readonly TimeSpan KeepAliveInterval;
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
-    public void Connect(IPEndPoint endPoint)
-    {
-        _taskScheduler.Schedule(async () =>
+        public WebSocketClientTransport(ITaskScheduler taskScheduler, IShamanLogger logger, TimeSpan keepAliveInterval)
         {
-            try
+            _taskScheduler = taskScheduler;
+            _logger = logger;
+            _clientWebSocket = new ClientWebSocket();
+            KeepAliveInterval = keepAliveInterval;
+        }
+
+        #region Client-side
+
+        public void Connect(IPEndPoint endPoint)
+        {
+            _taskScheduler.Schedule(async () =>
             {
-                var uri = new Uri($"ws://{endPoint}");
-                _logger.Info($"WsClient connecting to {uri}");
-                await _clientWebSocket.ConnectAsync(uri,
-                    CancellationToken.None // todo possible handle timeout
-                );
-                if (_clientWebSocket.State == WebSocketState.Open)
-                    OnConnected?.Invoke(endPoint);
-                await SendHb();
-                var cancellationToken = new CancellationTokenSource();
-                while (_clientWebSocket.State == WebSocketState.Open)
+                try
                 {
-                    var buffer = ArrayPool<byte>.Shared.Rent(1024 * 4);
-                    cancellationToken.CancelAfter(KeepAliveInterval);
-                    var result =
-                        await _clientWebSocket.ReceiveAsync(buffer, cancellationToken.Token);
-                    // _logger.Error($"result: {result.EndOfMessage} {result.Count} {result.MessageType} {result.CloseStatus} {result.CloseStatusDescription}");
-
-                    if (result.MessageType == WebSocketMessageType.Close)
-                        break;
-
-                    if (result.MessageType == WebSocketMessageType.Text)
+                    var uri = new Uri($"ws://{endPoint}");
+                    _logger.Info($"WsClient connecting to {uri}");
+                    await _clientWebSocket.ConnectAsync(uri,
+                        CancellationToken.None // todo possible handle timeout
+                    );
+                    if (_clientWebSocket.State == WebSocketState.Open)
+                        OnConnected?.Invoke(endPoint);
+                    await SendHb();
+                    var cancellationToken = new CancellationTokenSource();
+                    while (_clientWebSocket.State == WebSocketState.Open)
                     {
-                        var ping = buffer.AsSpan(0, result.Count).SequenceEqual(PingPongLetter);
-                        if (!ping)
+                        var buffer = ArrayPool<byte>.Shared.Rent(1024 * 4);
+                        cancellationToken.CancelAfter(KeepAliveInterval);
+                        var result =
+                            await _clientWebSocket.ReceiveAsync(buffer, cancellationToken.Token);
+                        // _logger.Error($"result: {result.EndOfMessage} {result.Count} {result.MessageType} {result.CloseStatus} {result.CloseStatusDescription}");
+
+                        if (result.MessageType == WebSocketMessageType.Close)
                             break;
 
-                        _rtt = (int) (DateTime.UtcNow - _pingSent).TotalMilliseconds;
-                        _waitForPong = false;
-                        _taskScheduler.Schedule(async () => { await SendHb(); }, Math.Max(0, (long) (1000 - _rtt)));
-                        continue;
-                    }
+                        if (result.MessageType == WebSocketMessageType.Text)
+                        {
+                            var ping = buffer.AsSpan(0, result.Count).SequenceEqual(PingPongLetter);
+                            if (!ping)
+                                break;
 
-                    if (result.MessageType != WebSocketMessageType.Binary)
-                    {
-                        _logger.Error($"Unsupported message type: {result.MessageType}");
-                        continue;
-                    }
+                            _rtt = (int) (DateTime.UtcNow - _pingSent).TotalMilliseconds;
+                            _waitForPong = false;
+                            _taskScheduler.Schedule(async () => { await SendHb(); }, Math.Max(0, (long) (1000 - _rtt)));
+                            continue;
+                        }
 
-                    var dataPacket = result.EndOfMessage
-                        ? new DataPacket(buffer, 0, result.Count, new DeliveryOptions(true, true))
-                        : await result.ReadBigMessage(_logger, buffer, _clientWebSocket);
-                    OnPacketReceived?.Invoke(endPoint, dataPacket,
-                        () => ArrayPool<byte>.Shared.Return(buffer)); // todo refactor buffer disposing to avoid closure
+                        if (result.MessageType != WebSocketMessageType.Binary)
+                        {
+                            _logger.Error($"Unsupported message type: {result.MessageType}");
+                            continue;
+                        }
+
+                        var dataPacket = result.EndOfMessage
+                            ? new DataPacket(buffer, 0, result.Count, new DeliveryOptions(true, true))
+                            : await result.ReadBigMessage(_logger, buffer, _clientWebSocket);
+                        OnPacketReceived?.Invoke(endPoint, dataPacket,
+                            () => ArrayPool<byte>.Shared.Return(buffer)); // todo refactor buffer disposing to avoid closure
+                    }
                 }
+                catch (Exception e)
+                {
+                    _logger.Error($"Peer connection processing failed: {e}");
+                    OnDisconnected?.Invoke(endPoint,
+                        new SimpleDisconnectInfo(ShamanDisconnectReason.ConnectionLost));
+                }
+            }, 0);
+        }
+
+        private async Task SendHb()
+        {
+            _pingSent = DateTime.UtcNow;
+            _waitForPong = true;
+            try
+            {
+                await SendImpl(PingPongLetter, WebSocketMessageType.Text);
             }
             catch (Exception e)
             {
-                _logger.Error($"Peer connection processing failed: {e}");
-                OnDisconnected?.Invoke(endPoint,
-                    new SimpleDisconnectInfo(ShamanDisconnectReason.ConnectionLost));
+                // no worries
             }
-        }, 0);
-    }
-
-    private async Task SendHb()
-    {
-        _pingSent = DateTime.UtcNow;
-        _waitForPong = true;
-        try
-        {
-            await SendImpl(PingPongLetter, WebSocketMessageType.Text);
         }
-        catch (Exception e)
-        {
-            // no worries
-        }
-    }
 
-    public void Send(byte[] buffer, int offset, int length, bool reliable, bool orderControl,
-        bool returnAfterSend = true)
-    {
-        _ = SendImpl(new ArraySegment<byte>(buffer, offset, length), WebSocketMessageType.Binary);
-    }
-
-    private async Task SendImpl(ArraySegment<byte> arraySegment, WebSocketMessageType messageType)
-    {
-        if (_clientWebSocket.State != WebSocketState.Open)
-            return;
-        var readOnlyMemory = arraySegment;
-        try
+        public void Send(byte[] buffer, int offset, int length, bool reliable, bool orderControl,
+            bool returnAfterSend = true)
         {
-            await _semaphore.WaitAsync();
-            await _clientWebSocket.SendAsync(readOnlyMemory, messageType,
-                true, CancellationToken.None);
+            _ = SendImpl(new ArraySegment<byte>(buffer, offset, length), WebSocketMessageType.Binary);
         }
-        catch (Exception e)
+
+        private async Task SendImpl(ArraySegment<byte> arraySegment, WebSocketMessageType messageType)
         {
             if (_clientWebSocket.State != WebSocketState.Open)
                 return;
-            _logger.Error($"Failed to send data to peer: {e}");
+            var readOnlyMemory = arraySegment;
+            try
+            {
+                await _semaphore.WaitAsync();
+                await _clientWebSocket.SendAsync(readOnlyMemory, messageType,
+                    true, CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                if (_clientWebSocket.State != WebSocketState.Open)
+                    return;
+                _logger.Error($"Failed to send data to peer: {e}");
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
         }
-        finally
+
+        private void HandleResult(Task sendingTask)
         {
-            _semaphore.Release();
+            if (!sendingTask.IsFaulted)
+                return;
+            if (_clientWebSocket.State != WebSocketState.Open)
+                return;
+            _logger.Error($"Failed to send data to peer: {sendingTask.Exception}");
         }
-    }
-
-    private void HandleResult(Task sendingTask)
-    {
-        if (!sendingTask.IsFaulted)
-            return;
-        if (_clientWebSocket.State != WebSocketState.Open)
-            return;
-        _logger.Error($"Failed to send data to peer: {sendingTask.Exception}");
-    }
 
 
-    public int GetPing()
-    {
-        if (_waitForPong)
+        public int GetPing()
         {
-            var rtt = (int) (DateTime.UtcNow - _pingSent).TotalMilliseconds;
-            return Math.Max(rtt, _rtt);
+            if (_waitForPong)
+            {
+                var rtt = (int) (DateTime.UtcNow - _pingSent).TotalMilliseconds;
+                return Math.Max(rtt, _rtt);
+            }
+
+            return _rtt;
         }
 
-        return _rtt;
+        public int GetRtt()
+        {
+            return GetPing();
+        }
+
+        public void Close()
+        {
+            if (_clientWebSocket.State == WebSocketState.Open)
+                _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None)
+                    .ContinueWith(HandleResult)
+                    .ContinueWith(_ =>
+                        OnDisconnected?.Invoke(null, new SimpleDisconnectInfo(ShamanDisconnectReason.PeerLeave)));
+        }
+
+        public void Close(byte[] data, int offset, int length)
+        {
+            if (_clientWebSocket.State == WebSocketState.Open)
+                _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                        Convert.ToBase64String(data, offset, length), CancellationToken.None)
+                    .ContinueWith(HandleResult)
+                    .ContinueWith(_ =>
+                    {
+                        OnDisconnected?.Invoke(null, new SimpleDisconnectInfo(ShamanDisconnectReason.PeerLeave));
+                    });
+        }
+
+
+        // why do we need IP here?
+        public int Mtu { get; }
+        public event Action<IPEndPoint, DataPacket, Action> OnPacketReceived;
+        public event Action<IPEndPoint> OnConnected;
+        public event Action<IPEndPoint, IDisconnectInfo> OnDisconnected;
+
+        #endregion
+
+        #region Server-side
+
+        public void AddEventCallbacks(Action<IPEndPoint, DataPacket, Action> onReceivePacket,
+            Func<IPEndPoint, bool> onConnect, Action<IPEndPoint, IDisconnectInfo> onDisconnect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Listen(int port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Tick()
+        {
+            throw new NotImplementedException("Tick is not required for WebSocket transport");
+        }
+
+        public bool IsTickRequired => false;
+
+        public void Send(IPEndPoint endPoint, byte[] buffer, int offset, int length, bool reliable, bool orderControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectPeer(IPEndPoint ipEndPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectPeer(IPEndPoint ipEndPoint, byte[] data, int offset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
-
-    public int GetRtt()
-    {
-        return GetPing();
-    }
-
-    public void Close()
-    {
-        if (_clientWebSocket.State == WebSocketState.Open)
-            _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None)
-                .ContinueWith(HandleResult)
-                .ContinueWith(_ =>
-                    OnDisconnected?.Invoke(null, new SimpleDisconnectInfo(ShamanDisconnectReason.PeerLeave)));
-    }
-
-    public void Close(byte[] data, int offset, int length)
-    {
-        if (_clientWebSocket.State == WebSocketState.Open)
-            _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
-                    Convert.ToBase64String(data, offset, length), CancellationToken.None)
-                .ContinueWith(HandleResult)
-                .ContinueWith(_ =>
-                {
-                    OnDisconnected?.Invoke(null, new SimpleDisconnectInfo(ShamanDisconnectReason.PeerLeave));
-                });
-    }
-
-
-    // why do we need IP here?
-    public int Mtu { get; }
-    public event Action<IPEndPoint, DataPacket, Action> OnPacketReceived;
-    public event Action<IPEndPoint> OnConnected;
-    public event Action<IPEndPoint, IDisconnectInfo> OnDisconnected;
-
-    #endregion
-
-    #region Server-side
-
-    public void AddEventCallbacks(Action<IPEndPoint, DataPacket, Action> onReceivePacket,
-        Func<IPEndPoint, bool> onConnect, Action<IPEndPoint, IDisconnectInfo> onDisconnect)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Listen(int port)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Tick()
-    {
-        throw new NotImplementedException("Tick is not required for WebSocket transport");
-    }
-
-    public bool IsTickRequired => false;
-
-    public void Send(IPEndPoint endPoint, byte[] buffer, int offset, int length, bool reliable, bool orderControl)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool DisconnectPeer(IPEndPoint ipEndPoint)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool DisconnectPeer(IPEndPoint ipEndPoint, byte[] data, int offset, int length)
-    {
-        throw new NotImplementedException();
-    }
-
-    #endregion
 }
